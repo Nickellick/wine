5b1709ae0da942f1783425fb2bbc58daec56e50f
Nick <kolya27.961@gmail.com>
2022-05-12 20:10:55 +0300

    Add feature of loading wine collection from excel

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
 
 excel_data_df = pandas.read_excel('wine.xlsx')
-print(excel_data_df.to_dict())
+wine_dict = excel_data_df.transpose().to_dict()
+print(wine_dict)
+
+wines = []
+
+for value in wine_dict.values():
+    wine = {}
+    wine['name'] = value['Название']
+    wine['variety'] = value['Сорт']
+    wine['price'] = value['Цена']
+    wine['image'] = value['Картинка']
+    wines.append(wine)
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -16,7 +28,7 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-rendered_page = template.render(since_years=f'{datetime.datetime.now().year - 1920}')
+rendered_page = template.render(since_years=f'{datetime.datetime.now().year - 1920}', wines=wines)
 
 with open('index.html', 'w', encoding='utf8') as file:
     file.write(rendered_page)
